4e15ef0f61f1865082db96de394be306d02cba1f
sognociel <sognociel@gmail.com>
2022-12-28 16:31:09 +0900

    feat: 상세 모니터링 슬라이스 작성 및 Item Spec 추가

--- src/Monitoring/pages/DetailItem.js
+++ src/Monitoring/pages/DetailItem.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { asyncItemSpec } from "../../store/detailItem/itemSpecSlice";
+import { asyncItemReport } from "../../store/detailItem/itemReportSlice";
+
 import LogContainer from "../container/LogContainer";
 import TimeLineContainer from "../container/TimeLineContainer";
 
 const DetailItem = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncItemSpec());
+    dispatch(asyncItemReport());
+  }, [dispatch]);
+
   return (
     <div>
       <TimeLineContainer />
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+// monitoring integration Reducer
 import totalChartReducer from "./totalChartSlice";
 import dateChartReducer from "./dateChartSlice";
 import currentChartReducer from "./currentChartSlice";
 import stateTableReducer from "./stateTableSlice";
 import errorFeedReducer from "./errorFeedSlice";
 
+// monitoring detailItem Reducer
+import itemReportReducer from "./detailItem/itemReportSlice";
+import itemSpecReducer from "./detailItem/itemSpecSlice";
+import usingLogReducer from "./detailItem/usingLogSlice";
+
 const store = configureStore({
   reducer: {
+    // monitoring integration
     totalChart: totalChartReducer,
     dateChart: dateChartReducer,
     currentChart: currentChartReducer,
     stateTable: stateTableReducer,
     errorFeed: errorFeedReducer,
+
+    // monitoring detailitem
+    itemReport: itemReportReducer,
+    itemSpec: itemSpecReducer,
+    usingLog: usingLogReducer,
   },
 });
 
