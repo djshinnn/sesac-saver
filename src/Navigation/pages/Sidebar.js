dd82c08e5d76db7ac250a2fa6210d64853758120
sognociel <sognociel@gmail.com>
2022-12-28 12:24:51 +0900

    style: 전체 레이아웃 수정

--- src/Navigation/pages/Sidebar.js
+++ src/Navigation/pages/Sidebar.js
@@ -20,115 +20,116 @@ const Navbar = () => {
         <Link to="#" className="menu-bars">
           <FaBars onClick={showSidebar} />
         </Link>
+
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items">
+            <li className="navbar-toggle" onClick={showSidebar}>
+              <Link to="#" className="menu-bars">
+                <AiOutlineClose />
+              </Link>
+            </li>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>✅모니터링</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div onClick={showSidebar}>
+                  <li>
+                    <Link to="/">통합 모니터링</Link>
+                  </li>
+                  <li>
+                    <Link to="charging">충전소 모니터링</Link>
+                  </li>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>✅충전기 관리</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div onClick={showSidebar}>
+                  <li>
+                    <Link to="/chargingstationlist">충전소 목록</Link>
+                  </li>
+                  <li>
+                    <Link to="/errorlist">충전기 장애 목록</Link>
+                  </li>
+                  <li>
+                    <Link to="/errorregist">충전기 장애 등록</Link>
+                  </li>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>✅충전 내역</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div onClick={showSidebar}>
+                  <li>
+                    <Link to="/stationhistory">충전소 기간별 내역</Link>
+                  </li>
+                  <li>
+                    <Link to="/memberhistory">회원 기간별 내역</Link>
+                  </li>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>✅회원 관리</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div onClick={showSidebar}>
+                  <li>
+                    <Link to="/memberlist">회원 목록</Link>
+                  </li>
+                  <li>
+                    <Link to="/membercardlist">회원 결제 카드 목록</Link>
+                  </li>
+                  <li>
+                    <Link to="/memberpaylist">회원 결제 내역</Link>
+                  </li>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>✅고객관리</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div onClick={showSidebar}>
+                  <li>
+                    <Link to="/notice">공지사항</Link>
+                  </li>
+                  <li>
+                    <Link to="/faq">FAQ</Link>
+                  </li>
+                  <li>
+                    <Link to="/faqedit">FAQ 등록/변경</Link>
+                  </li>
+                  <li>
+                    <Link to="/privatepolicy">개인정보 방침</Link>
+                  </li>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>✅마이페이지</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div onClick={showSidebar}>
+                  <li>
+                    <Link to="/mypage">비밀번호 수정</Link>
+                  </li>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+          </ul>
+        </nav>
       </div>
-      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-        <ul className="nav-menu-items">
-          <li className="navbar-toggle" onClick={showSidebar}>
-            <Link to="#" className="menu-bars">
-              <AiOutlineClose />
-            </Link>
-          </li>
-          <Accordion>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography>✅모니터링</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <div onClick={showSidebar}>
-                <li>
-                  <Link to="/">통합 모니터링</Link>
-                </li>
-                <li>
-                  <Link to="charging">충전소 모니터링</Link>
-                </li>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography>✅충전기 관리</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <div onClick={showSidebar}>
-                <li>
-                  <Link to="/chargingstationlist">충전소 목록</Link>
-                </li>
-                <li>
-                  <Link to="/errorlist">충전기 장애 목록</Link>
-                </li>
-                <li>
-                  <Link to="/errorregist">충전기 장애 등록</Link>
-                </li>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography>✅충전 내역</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <div onClick={showSidebar}>
-                <li>
-                  <Link to="/stationhistory">충전소 기간별 내역</Link>
-                </li>
-                <li>
-                  <Link to="/memberhistory">회원 기간별 내역</Link>
-                </li>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography>✅회원 관리</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <div onClick={showSidebar}>
-                <li>
-                  <Link to="/memberlist">회원 목록</Link>
-                </li>
-                <li>
-                  <Link to="/membercardlist">회원 결제 카드 목록</Link>
-                </li>
-                <li>
-                  <Link to="/memberpaylist">회원 결제 내역</Link>
-                </li>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography>✅고객관리</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <div onClick={showSidebar}>
-                <li>
-                  <Link to="/notice">공지사항</Link>
-                </li>
-                <li>
-                  <Link to="/faq">FAQ</Link>
-                </li>
-                <li>
-                  <Link to="/faqedit">FAQ 등록/변경</Link>
-                </li>
-                <li>
-                  <Link to="/privatepolicy">개인정보 방침</Link>
-                </li>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography>✅마이페이지</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <div onClick={showSidebar}>
-                <li>
-                  <Link to="/mypage">비밀번호 수정</Link>
-                </li>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-        </ul>
-      </nav>
     </>
   );
 };
