bd23f33ef68e14c72bc7cf898d6c9629b307563b
Harimad <leesh68481@gmail.com>
2023-01-03 15:06:15 +0900

    style: List 컴포넌트 스타일 추가

--- src/common/FormElements/List.js
+++ src/common/FormElements/List.js
@@ -1,10 +1,56 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { DataGrid } from "@mui/x-data-grid";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import { chargingStationActions } from "../../store/chargingStation/stationListSlice";
 import { asyncChargerListData } from "../../store/chargingStation/chargerListSlice";
 import Title from "./Title";
+
+const theme = createTheme({
+  components: {
+    MuiDataGrid: {
+      styleOverrides: {
+        cell: {
+          fontSize: "1.6rem",
+        },
+        columnHeaderTitle: {
+          fontSize: "1.8rem",
+        },
+      },
+    },
+    MuiButtonBase: {
+      styleOverrides: {
+        root: {
+          color: "#311b92",
+        },
+      },
+    },
+    MuiMenuItem: {
+      styleOverrides: {
+        root: {
+          fontSize: "1.8rem",
+        },
+      },
+    },
+    MuiTablePagination: {
+      styleOverrides: {
+        selectLabel: {
+          fontSize: "1.8rem",
+        },
+        toolbar: {
+          fontSize: "1.8rem",
+        },
+        menuItem: {
+          fontSize: "1.8rem",
+        },
+        displayedRows: {
+          fontSize: "1.8rem",
+        },
+      },
+    },
+  },
+});
 
 const List = ({ rows, columns, height, page, title, station }) => {
   const dispatch = useDispatch();
@@ -21,15 +67,17 @@ const List = ({ rows, columns, height, page, title, station }) => {
   return (
     <div className="list">
       <Title title={title} />
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={pageSize}
-        rowsPerPageOptions={[5, 10, 15, 20]}
-        onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-        sx={{ fontSize: "1.7rem", height }}
-        onCellClick={clickHandler}
-      />
+      <ThemeProvider theme={theme}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={pageSize}
+          rowsPerPageOptions={[5, 10, 15, 20]}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          sx={{ fontSize: "1.7rem", height }}
+          onCellClick={clickHandler}
+        />
+      </ThemeProvider>
     </div>
   );
 };
